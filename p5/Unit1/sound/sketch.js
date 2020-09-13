fce228a963cdffdf06b21e12d7cd78e474826d44
jradam3 <58442724+jradam3@users.noreply.github.com>
2020-09-13 14:36:33 -0500

    Add files via upload

---
+++ p5/Unit1/sound/sketch.js
@@ -0,0 +1,27 @@
+var song1;
+
+function preload() {
+  song1 = loadSound('assets/MetaVitaIntro.mp3');
+}
+
+function setup() {
+  createCanvas(500, 500)  ;
+  song1.loop();
+
+}
+
+function touchStarted() {
+  getAudioContext().resume();
+}
+
+function draw() {
+  background(100);
+}
+
+function mouseReleased() {
+  if (song1.isPlaying()) {
+    song1.pause() ;
+  } else {
+    song1.loop()  ;
+  }
+}
