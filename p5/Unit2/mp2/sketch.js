e5c5e6c08fb6956286a737dd1ff4d77ffd896388
jradam3 <58442724+jradam3@users.noreply.github.com>
2020-10-08 09:21:09 -0500

    Add files via upload

---
+++ p5/Unit2/mp2/sketch.js
@@ -0,0 +1,55 @@
+var img1, img2, img3;
+let state = 0;
+let timer = 0;
+
+function setup() {
+  createCanvas(800, 800);
+
+  img1 = loadImage('assets/1.png');
+  img2 = loadImage('assets/2.png');
+  img3 = loadImage('assets/3.png');
+
+    imageMode(CENTER)
+}
+
+function draw() {
+
+  switch(state) {
+      case 0:
+      background(100);
+      image(img1, width/2, height/2+210, 400, 200);
+
+      timer = timer + 1;
+      if (timer > 5 * 60){
+        state = 1;
+      timer = 0;
+      }
+      break;
+
+      case 1:
+      background ('black');
+      image(img2, width/2, height/2+210, 400, 200);
+
+      timer = timer + 1;
+      if (timer > 5 * 60){
+        state = 2;
+      timer = 0;
+      }
+      break;
+
+      case 2:
+      background ('red');
+      image(img3, width/2, height/2+210, 400, 200);
+      break;
+
+  }
+}
+
+function mouseReleased() {
+  state++;
+
+  if (state > 2){
+    state = 0;
+  }
+
+}
